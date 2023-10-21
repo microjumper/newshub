fff9eda9f6a0e7b6f9818e8faee2d502dc3eb143
Salvatore Sammarco <microjmpr@gmail.com>
2023-10-21 18:56:43 +0200

    remove environments

--- src/app/services/news/news.service.ts
+++ src/app/services/news/news.service.ts
@@ -5,17 +5,15 @@ import {Observable, tap} from "rxjs";
 
 import { Article } from "../../types/article.type";
 
-import { environment } from "../../../environments/environment.development";
-
 @Injectable({
   providedIn: 'root'
 })
 export class NewsService {
-
+  private readonly getArticlesEndpoint : string = "https://newshubfunction.azurewebsites.net/api/articles?code=WaFF2-ScGyre7gMxSVpo-PyESbICsHldN9rRwR9WGH8JAzFuHS_gJA=="
   constructor(private httpClient: HttpClient) { }
 
   getArticles(): Observable<Article[]> {
-    return this.httpClient.get<Article[]>(environment.getArticlesEndpoint).pipe(
+    return this.httpClient.get<Article[]>(this.getArticlesEndpoint).pipe(
       tap(articles => {
         articles.forEach(a => {
           if(!a.urlToImage || a.urlToImage.length === 0) {
--- src/environments/environment.development.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  getArticlesEndpoint: "http://localhost:7071/api/articles"
-};

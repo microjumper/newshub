706aece485aa06197d0ab570296185ca2c7e36e1
Salvatore Sammarco <microjmpr@gmail.com>
2023-10-22 10:38:54 +0200

    remove ngsrc directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from "@angular/common/http";
+import { HttpClientModule } from "@angular/common/http";
 
-import {InputTextModule} from "primeng/inputtext";
+import { InputTextModule } from "primeng/inputtext";
+import { CardModule } from "primeng/card";
+import { DataViewModule } from "primeng/dataview";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { ArticleComponent } from './components/article/article.component';
-import {CardModule} from "primeng/card";
-import {DataViewModule} from "primeng/dataview";
-import {NgOptimizedImage} from "@angular/common";
 
 @NgModule({
   declarations: [
@@ -24,8 +23,7 @@ import {NgOptimizedImage} from "@angular/common";
     HttpClientModule,
     InputTextModule,
     CardModule,
-    DataViewModule,
-    NgOptimizedImage
+    DataViewModule
   ],
   providers: [],
   bootstrap: [AppComponent]
